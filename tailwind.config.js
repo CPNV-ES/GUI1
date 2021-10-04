d845aa3d5f39943d296fea56d3faacc21b33c3ab
HDubuis <helene.dubuis@cpnv.ch>
2021-10-04 10:30:18 +0200

    Add variable and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+      extend: {
+          colors: {
+              main: colors.white,
+              second: '#4DC591',
+              third: '#F6F6F5',
+              fourth: '#BCC1CD',
+              mainDark: '#202225',
+              secondDark: '#4DC591',
+              thirdDark: '#2F3136',
+              fourthDark: colors.white,
+          }
+      },
     fontFamily: {
       sans: ['Roboto', 'sans-serif'],
     },
-    extend: {},
   },
   variants: {
     extend: {
