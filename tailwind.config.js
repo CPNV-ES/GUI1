4d5f4462c7700f0c536dc80de1f44afaee916090
Penfu <sypenfu@gmail.com>
2021-11-07 21:54:39 +0100

    PWA

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,52 @@ module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-      extend: {
-          colors: {
-              white: '#BCC1CD',
-              black: '#191919',
-              main: '#F0F0F0',
-              second: '#4DC591',
-              third: '#EBEBEB',
-              fourth: '#BCC1CD',
-              mainDark: '#202225',
-              secondDark: '#4DC591',
-              thirdDark: '#2F3136',
-              fourthDark: "#F0F0F0",
-          }
+    extend: {
+      colors: {
+        white: '#BCC1CD',
+        black: '#191919',
+        main: '#F0F0F0',
+        second: '#4DC591',
+        third: '#EBEBEB',
+        fourth: '#BCC1CD',
+        mainDark: '#202225',
+        secondDark: '#4DC591',
+        thirdDark: '#2F3136',
+        fourthDark: '#F0F0F0'
       },
-    fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
-    },
+      keyframes: {
+        fade: {
+          '0%': {
+            opacity: '0'
+          },
+          '100%': {
+            opacity: '1'
+          }
+        },
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        }
+      },
+      animation: {
+        fade: 'fade 0.5s ease-out',
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      }
+    }
+  },
+  fontFamily: {
+    sans: ['Roboto', 'sans-serif']
   },
   variants: {
     extend: {
       display: ['dark']
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
