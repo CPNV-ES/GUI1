2de2b4f9b0c03237e967dae0ac5203c6ac84ddf1
MARECHAL Armand <Armand.MARECHAL@cpnv.ch>
2021-11-08 09:50:25 +0100

    Refacto

--- public/resource/js/course/absences.js
+++ public/resource/js/course/absences.js
@@ -1,16 +1,24 @@
+var course = JSON.parse(localStorage.course);
+
+$('#course-name').text(course['name']);
+$('#course-class').text(course['class']);
+$('#course-room').text(course['room']);
+$('#course-teacher').text(course['teacher']);
+$('#course-time').text(course['startTime'] + ' - ' + course['endTime']);
+
 $('#btn-back').click(function () {
   $('#modal-content').html('')
   $('#modal-content').load('components/course-info.html')
 })
 
 $('.btn-student').click(function () {
-  var element = $(this)
+  var element = $(this);
 
   if ($('#absences-management').length) {
-    $('#absences-management').remove()
+    $('#absences-management').remove();
   }
 
   $.get('components/course-manage-absences.html', function (data) {
-    element.append(data)
+    element.append(data);
   })
 })
